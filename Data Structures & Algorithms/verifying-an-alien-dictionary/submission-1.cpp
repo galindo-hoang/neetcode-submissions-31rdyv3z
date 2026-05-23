427884a0dcd0fc57ae00b4f104e00cf8fa21446f
huy <69897697+ZoeHoag@users.noreply.github.com>
2026-05-23 15:46:17 +0700

    Add: verifying-an-alien-dictionary - submission-1

---
+++ Data Structures & Algorithms/verifying-an-alien-dictionary/submission-1.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    bool isAlienSorted(vector<string>& words, string order) {
+        vector<int> store(order.size(),0);
+        for(int i = 0; i < order.size(); ++i) {
+            store[order[i] - 'a'] = i;
+        }
+        int maxSize = 0;
+        for(string& w: words) {
+            maxSize = max(maxSize,(int) w.size());
+        }
+        int j = 0;
+        for(int i = 0; i < maxSize; ++i) {
+            int temp = -1;
+            bool isGreater = true;
+            for(int k = j; k < words.size(); ++k) {
+                if(words[k].size() <= i) {
+                    if(temp != -1) return false;
+                    continue;
+                }
+                if(temp == -1){
+                    temp = store[words[k][i] - 'a'];
+                }
+                else {
+                    if(temp == (store[words[k][i] - 'a'])) {
+                        if(isGreater) j = k - 1;
+                        isGreater = false;
+                    } else if(temp > (store[words[k][i] - 'a'])) {
+                        return false;
+                    } else temp = store[words[k][i] - 'a'];
+                }
+            }
+            if (isGreater) return true;
+        }
+        return true;
+    }
+};
